2f3dcfa29b7c12c1daaf8b3a1d032a09fd288a80
Sri <83589407+realsri@users.noreply.github.com>
2026-09-16 22:27:50 +0300

    A Tour of Go: Basics

--- A_tour_of_Go/hello.go
+++ A_tour_of_Go/hello.go
@@ -1,8 +1,18 @@
-// always declare the package first
-package main
+// Everything is a package in Go //Although groups of packages can be managed together as a module using go.mod
+package main //main is the "executable package" //And everything else is a "Library package"
+// All go files inside a single directory must share the exact same package name
+// They can access each other's variables and functions freely without importing
 
-import "fmt" //import required packages
+// always start your programs with your package declaration
 
-func main() { //program's entry point
-	fmt.Println("Hello, I am ஶ்ரீ")
+import "fmt"
+
+// then import the required packages
+
+// then continue with your list of variables, constants, functions in no strict order
+
+// then main() - program's entry point
+
+func main() {
+	fmt.Println("Hello 👋, I am ஶ்ரீ")
 }
--- A_tour_of_Go/sandbox.go
+++ A_tour_of_Go/sandbox.go
@@ -8,9 +8,20 @@ import (
 func main() {
 	fmt.Print("Welcome to whatever it is")
 	fmt.Print("\n")
+	// for new line
+
 	now := time.Now()
+
 	fmt.Print("The current time is ", now)
+	// no space between arguments and no new line at the end
+
 	fmt.Println()
+	// for new line
+
 	fmt.Println(now.Month(), now.Day(), now.Year())
+	// print spaces between the arguments and a new line as well at the end
+
 	fmt.Printf("%02d:%02d:%02d\n", now.Hour(), now.Minute(), now.Second())
+	// formatted printing
+	// %02d - format as min 2 decimal integers, use 0 to pad any empty spaces
 }
